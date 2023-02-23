bfb6aaa7689c8b00451d2fa870e7d10147f122c8
NikolaPehnec <nikola.pehnec12345@gmail.com>
2023-02-23 23:37:30 +0100

    Initial commit

---
+++ app/src/main/java/sofascore/academy/sofaproject/FirstFragment.kt
@@ -0,0 +1,56 @@
+package sofascore.academy.sofaproject
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import sofascore.academy.sofaproject.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListeners()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setListeners() {
+        binding.buttonHideShowText.setOnClickListener {
+            binding.helloTextView.isVisible = !binding.helloTextView.isVisible
+
+            if (binding.buttonHideShowText.text == getText(R.string.hide))
+                binding.buttonHideShowText.text = getText(R.string.show)
+            else
+                binding.buttonHideShowText.text = getText(R.string.hide)
+        }
+
+        binding.buttonChangeText.setOnClickListener {
+            if (binding.helloTextView.text == getText(R.string.hello_text))
+                binding.helloTextView.text = getText(R.string.hello_text2)
+            else
+                binding.helloTextView.text = getText(R.string.hello_text)
+        }
+    }
+}
