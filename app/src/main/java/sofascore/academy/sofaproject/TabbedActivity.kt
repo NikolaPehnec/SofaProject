83c2541ade01abe4a2c0ea0a3a9a040064acc3c7
NikolaPehnec <nikola.pehnec12345@gmail.com>
2023-03-03 16:26:15 +0100

    Format code

--- app/src/main/java/sofascore/academy/sofaproject/TabbedActivity.kt
+++ app/src/main/java/sofascore/academy/sofaproject/TabbedActivity.kt
@@ -6,7 +6,7 @@ import androidx.viewpager2.widget.ViewPager2
 import sofascore.academy.sofaproject.databinding.ActivityTabbedBinding
 import sofascore.academy.sofaproject.ui.main.SectionsPagerAdapter
 
-//testing
+// testing
 class TabbedActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTabbedBinding
 
