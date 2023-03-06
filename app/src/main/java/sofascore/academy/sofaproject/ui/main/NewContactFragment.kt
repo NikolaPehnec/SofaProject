45cf220e443820ead5612c7384f638a4fa574f90
NikolaPehnec <nikola.pehnec12345@gmail.com>
2023-03-06 16:47:59 +0100

    Optimizing validation and clearFields

--- app/src/main/java/sofascore/academy/sofaproject/ui/main/NewContactFragment.kt
+++ app/src/main/java/sofascore/academy/sofaproject/ui/main/NewContactFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import sofascore.academy.sofaproject.R
 import sofascore.academy.sofaproject.data.Person
 import sofascore.academy.sofaproject.databinding.FragmentNewContactBinding
@@ -15,6 +18,7 @@ class NewContactFragment : Fragment() {
     private val peopleViewModel: PeopleViewModel by activityViewModels()
     private var _binding: FragmentNewContactBinding? = null
     private val binding get() = _binding!!
+    private val editTextFields = mutableListOf<TextInputEditText>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,14 @@ class NewContactFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentNewContactBinding.inflate(inflater, container, false)
+
+        binding.newPersonLinearLayout.children.filterIsInstance(TextInputLayout::class.java)
+            .forEach { txtInptLayout ->
+                txtInptLayout.editText?.let {
+                    editTextFields.add(it as TextInputEditText)
+                }
+            }
+
         return binding.root
     }
 
@@ -49,18 +61,7 @@ class NewContactFragment : Fragment() {
         val nationality = binding.nationality.text.toString()
         val additionalInfo = binding.additionalInfo.text.toString()
 
-        if (validateData(
-                firstName,
-                lastName,
-                age,
-                oib,
-                gender,
-                education,
-                fatherName,
-                motherName,
-                nationality
-            )
-        ) {
+        if (validateData()) {
             peopleViewModel.addPerson(
                 Person(
                     firstName,
@@ -84,54 +85,14 @@ class NewContactFragment : Fragment() {
     /**
      * Basic validation
      */
-    fun validateData(
-        firstName: String,
-        lastName: String,
-        age: String,
-        oib: String,
-        gender: String,
-        education: String,
-        motherName: String,
-        fatherName: String,
-        nationality: String
-    ): Boolean {
+    fun validateData(): Boolean {
         var validated = true
 
-        if (firstName.isEmpty()) {
-            binding.firstName.error = getString(R.string.missing_field_validation)
-            validated = false
-        }
-        if (lastName.isEmpty()) {
-            binding.lastName.error = getString(R.string.missing_field_validation)
-            validated = false
-        }
-        if (age.isEmpty()) {
-            binding.age.error = getString(R.string.missing_field_validation)
-            validated = false
-        }
-        if (oib.isEmpty()) {
-            binding.oib.error = getString(R.string.missing_field_validation)
-            validated = false
-        }
-        if (gender.isEmpty()) {
-            binding.gender.error = getString(R.string.missing_field_validation)
-            validated = false
-        }
-        if (education.isEmpty()) {
-            binding.education.error = getString(R.string.missing_field_validation)
-            validated = false
-        }
-        if (motherName.isEmpty()) {
-            binding.motherName.error = getString(R.string.missing_field_validation)
-            validated = false
-        }
-        if (fatherName.isEmpty()) {
-            binding.fatherName.error = getString(R.string.missing_field_validation)
-            validated = false
-        }
-        if (nationality.isEmpty()) {
-            binding.nationality.error = getString(R.string.missing_field_validation)
-            validated = false
+        editTextFields.forEach {
+            if (it.text.toString().isEmpty()) {
+                it.error = getString(R.string.missing_field_validation)
+                validated = false
+            }
         }
 
         return validated
@@ -143,16 +104,9 @@ class NewContactFragment : Fragment() {
     }
 
     private fun clearFields() {
-        binding.firstName.text?.clear()
-        binding.lastName.text?.clear()
-        binding.age.text?.clear()
-        binding.oib.text?.clear()
-        binding.gender.text?.clear()
-        binding.education.text?.clear()
-        binding.fatherName.text?.clear()
-        binding.motherName.text?.clear()
-        binding.nationality.text?.clear()
-        binding.additionalInfo.text?.clear()
+        editTextFields.forEach {
+            it.text?.clear()
+        }
 
         binding.firstName.requestFocus()
     }
