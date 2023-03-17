248f9e6b992698f91ed2a06da7e7b10c36b3638b
NikolaPehnec <nikola.pehnec12345@gmail.com>
2023-03-17 01:16:29 +0100

    Dimming status bar

--- app/src/main/java/sofascore/academy/sofaproject/BottomNavigationActivity.kt
+++ app/src/main/java/sofascore/academy/sofaproject/BottomNavigationActivity.kt
@@ -1,10 +1,12 @@
 package sofascore.academy.sofaproject
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import sofascore.academy.sofaproject.databinding.ActivityBottomNavigationBinding
+
 
 // testing
 class BottomNavigationActivity : AppCompatActivity() {
@@ -13,6 +15,8 @@ class BottomNavigationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE
 
         binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
