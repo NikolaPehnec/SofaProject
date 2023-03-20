2cc6cba2d17725938201a30e6cb08b5328027dc5
NikolaPehnec <nikola.pehnec12345@gmail.com>
2023-03-20 17:00:06 +0100

    Reusing fighter_row, no need for second viewHolder?

--- app/src/main/java/sofascore/academy/sofaproject/view/adapters/FighterRecyclerAdapter.kt
+++ app/src/main/java/sofascore/academy/sofaproject/view/adapters/FighterRecyclerAdapter.kt
@@ -2,6 +2,7 @@ package sofascore.academy.sofaproject.view.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.RecyclerView
@@ -11,7 +12,6 @@ import sofascore.academy.sofaproject.R
 import sofascore.academy.sofaproject.data.Coach
 import sofascore.academy.sofaproject.data.DataTypes
 import sofascore.academy.sofaproject.data.Fighter
-import sofascore.academy.sofaproject.databinding.CoachRowBinding
 import sofascore.academy.sofaproject.databinding.FighterRowBinding
 
 class FighterRecyclerAdapter(
@@ -19,38 +19,23 @@ class FighterRecyclerAdapter(
     private var items: MutableList<Any>,
     private val listener: OnItemClickListener
 ) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    RecyclerView.Adapter<FighterRecyclerAdapter.ViewHolderFighter>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
-            DataTypes.FIGHTER.ordinal ->
-                ViewHolderFighter(
-                    FighterRowBinding.inflate(
-                        LayoutInflater.from(parent.context),
-                        parent,
-                        false
-                    )
-                )
-            DataTypes.COACH.ordinal ->
-                ViewHolderCoach(
-                    CoachRowBinding.inflate(
-                        LayoutInflater.from(parent.context),
-                        parent,
-                        false
-                    )
-                )
-            else -> throw IllegalArgumentException("Invalid view type")
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderFighter {
+        return ViewHolderFighter(
+            FighterRowBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolderFighter, position: Int) {
         holder.itemView.animation =
             AnimationUtils.loadAnimation(holder.itemView.context, R.anim.animation_recycler)
 
-        when (holder) {
-            is ViewHolderFighter -> holder.bind(items[position] as Fighter)
-            is ViewHolderCoach -> holder.bind(items[position] as Coach)
-        }
+        holder.bind(items[position])
     }
 
     fun getNumberOfItems(): Int = items.size
@@ -86,44 +71,46 @@ class FighterRecyclerAdapter(
     inner class ViewHolderFighter(private val binding: FighterRowBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: Fighter) {
-            binding.figterName.text =
-                context.getString(R.string.fighter_name, item.firstName, item.lastName)
-            binding.figterScore.text =
-                context.getString(R.string.fighter_score, item.win, item.draw, item.lose)
+        fun bind(item: Any) {
+            when (item) {
+                is Fighter -> {
+                    binding.figterName.text =
+                        context.getString(R.string.fighter_name, item.firstName, item.lastName)
+                    binding.figterScore.text =
+                        context.getString(R.string.fighter_score, item.win, item.draw, item.lose)
+                    binding.coachLabel.visibility = View.INVISIBLE
 
-            val imgRequest = ImageRequest.Builder(context)
-                .data(item.imageUrl.toString())
-                .placeholder(R.drawable.person_placeholder)
-                .error(R.drawable.person_placeholder)
-                .target(binding.figterImage)
-                .build()
-            ImageLoader.Builder(context).build().enqueue(imgRequest)
+                    val imgRequest = ImageRequest.Builder(context)
+                        .data(item.imageUrl.toString())
+                        .placeholder(R.drawable.person_placeholder)
+                        .error(R.drawable.person_placeholder)
+                        .target(binding.figterImage)
+                        .build()
+                    ImageLoader.Builder(context).build().enqueue(imgRequest)
 
-            binding.root.setOnClickListener {
-                listener.onItemClick(items[bindingAdapterPosition] as Fighter)
-            }
-        }
-    }
+                    binding.root.setOnClickListener {
+                        listener.onItemClick(items[bindingAdapterPosition] as Fighter)
+                    }
+                }
 
-    inner class ViewHolderCoach(private val binding: CoachRowBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+                is Coach -> {
+                    binding.figterName.text =
+                        context.getString(R.string.fighter_name, item.firstName, item.lastName)
+                    binding.figterScore.text = item.speciality
+                    binding.coachLabel.visibility = View.VISIBLE
 
-        fun bind(item: Coach) {
-            binding.coachName.text =
-                context.getString(R.string.fighter_name, item.firstName, item.lastName)
-            binding.coachSpeciality.text = item.speciality
+                    val imgRequest = ImageRequest.Builder(context)
+                        .data(item.imageUrl.toString())
+                        .placeholder(R.drawable.person_placeholder)
+                        .error(R.drawable.person_placeholder)
+                        .target(binding.figterImage)
+                        .build()
+                    ImageLoader.Builder(context).build().enqueue(imgRequest)
 
-            val imgRequest = ImageRequest.Builder(context)
-                .data(item.imageUrl.toString())
-                .placeholder(R.drawable.person_placeholder)
-                .error(R.drawable.person_placeholder)
-                .target(binding.coachImage)
-                .build()
-            ImageLoader.Builder(context).build().enqueue(imgRequest)
-
-            binding.root.setOnClickListener {
-                listener.onItemClick(items[bindingAdapterPosition] as Coach)
+                    binding.root.setOnClickListener {
+                        listener.onItemClick(items[bindingAdapterPosition] as Coach)
+                    }
+                }
             }
         }
     }
